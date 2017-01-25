7bd5fdcd37b37a26db19f14d31a6d19fe7e1adce
rtnolasco <rtnolasco@hotmail.com>
2017-01-25 22:58:16 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+/*
+This footer.php file will be included at the end of all files so that we won’t have to write the same footer codes every-time.
+*/
+<div class="container">
+ <div class="alert alert-info">
+    <strong>tutorial !</strong> <a href="http://www.codingcage.com/">Coding Cage</a>!
+ </div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
